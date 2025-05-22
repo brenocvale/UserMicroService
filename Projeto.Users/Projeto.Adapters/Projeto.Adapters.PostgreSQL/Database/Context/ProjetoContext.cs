74ab7bec9b7afc3b0ef2a0b5a1f2db92992ba6fa
brenocvale <brenoconceicaovale@gmail.com>
2025-05-22 18:33:54 -0300

    Feature(): Preparing to settings

--- Projeto.Users/Projeto.Adapters/Projeto.Adapters.PostgreSQL/Database/Context/ProjetoContext.cs
+++ Projeto.Users/Projeto.Adapters/Projeto.Adapters.PostgreSQL/Database/Context/ProjetoContext.cs
@@ -2,8 +2,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Projeto.Adapters.PostgreSQL.Database.Context
 {
-    public class ProjetoContext(DbContextOptions options): DbContext(options)
+    public class ProjetoContext : DbContext
     {
+        private readonly DbContextOptions _options;
+        public ProjetoContext(DbContextOptions<ProjetoContext> options) : base(options)
+        => _options = options; 
+        
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
--- Projeto.Users/Projeto.Adapters/Projeto.Adapters.PostgreSQL/Database/Settings/UpdatingTimestamp.cs
+++ Projeto.Users/Projeto.Adapters/Projeto.Adapters.PostgreSQL/Database/Settings/UpdatingTimestamp.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Projeto.Adapters.PostgreSQL.Database.Settings
 {
     public class UpdatingTimestamp
